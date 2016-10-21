e1085a743b7efc4435173153918f5a182732702a
jtumms <john.tumminelli@tummsmedia.com>
2016-10-21 12:30:56 -0400

    Initial

---
+++ src/main/java/com/example/Hurricane.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import javax.persistence.*;
+
+/**
+ * Created by zach on 10/21/16.
+ */
+@Entity
+@Table(name = "hurricanes")
+public class Hurricane {
+    enum Category {
+        ONE, TWO, THREE, FOUR, FIVE
+    }
+
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    String location;
+
+    @Column(nullable = false)
+    Category category;
+
+    @Column(nullable = false)
+    String image;
+
+    public Hurricane() {
+    }
+
+    public Hurricane(String name, String location, Category category, String image) {
+        this.name = name;
+        this.location = location;
+        this.category = category;
+        this.image = image;
+    }
+}
---
+++ src/main/java/com/example/HurricaneRepository.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by zach on 10/21/16.
+ */
+public interface HurricaneRepository extends CrudRepository<Hurricane, Integer> {
+}
