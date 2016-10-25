4d8d61c530d36670998832f3b1015780fa250aba
jtumms <john.tumminelli@tummsmedia.com>
2016-10-25 16:17:21 -0400

    Fixed and debugged

--- src/main/java/com/example/HurricaneRepository.java
+++ src/main/java/com/example/HurricaneRepository.java
@@ -8,10 +8,10 @@ import java.util.List;
  * Created by zach on 10/21/16.
  */
 public interface HurricaneRepository extends CrudRepository<Hurricane, Integer> {
-    List<Hurricane> findByCategory(Hurricane.Category category);
-    List<Hurricane> findByLocation(String location);
-    List<Hurricane> findByNameContainingIgnoreCaseOrLocationContainingIgnoreCase(String search, String search1);
-    List<Hurricane> findByOrderByDateDesc();
+    public List<Hurricane> findByCategory(Hurricane.Category category);
+    public List<Hurricane> findByLocation(String location);
+    public List<Hurricane> findByNameContainingIgnoreCaseOrLocationContainingIgnoreCase(String search, String search1);
+    public List<Hurricane> findByOrderByDateDesc();
 
     Hurricane findFirstByLocation(String location);
     int countByCategory(Hurricane.Category category);
